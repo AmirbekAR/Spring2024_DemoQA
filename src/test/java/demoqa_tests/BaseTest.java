f8aa2b23f18e8d792ab7f89b95896d53ade26cd7
Amirbek <arynbaevamirbisnes@gmail.com>
2024-10-31 16:03:45 +0600

    some changes for test

--- src/test/java/demoqa_tests/BaseTest.java
+++ src/test/java/demoqa_tests/BaseTest.java
@@ -4,34 +4,47 @@ import demoqa.drivers.DriverManager;
 import demoqa.helper.WebElementActions;
 import demoqa.modul_import.ImportHelpers;
 import demoqa.pages.SelectMenuPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-    public abstract class BaseTest {
+import java.time.Duration;
 
-        public WebDriver driver;
-        public WebElementActions webElementActions;
-        public ImportHelpers helper;
-        public demoqa.module_import.DemoqaPages demoqaPages;
-        public SelectMenuPage selectMenuPage;
+public abstract class BaseTest {
 
+    public WebDriver driver;
+    public WebElementActions webElementActions;
+    public ImportHelpers helper;
+    public demoqa.module_import.DemoqaPages demoqaPages;
+    public SelectMenuPage selectMenuPage;
+    private WebDriverWait wait; // Добавляем переменную для ожидания
 
-        @BeforeClass(alwaysRun = true)
-        public void setUp() {
-            driver = DriverManager.getDriver();
-            if (driver == null) {
-                throw new IllegalArgumentException("Driver must be set");
-            }
-            webElementActions = new WebElementActions();
-            helper = new ImportHelpers(driver);
-            demoqaPages = new demoqa.module_import.DemoqaPages();
-            webElementActions = new WebElementActions();
-            selectMenuPage = new SelectMenuPage();
+    @BeforeClass(alwaysRun = true)
+    public void setUp() {
+        driver = DriverManager.getDriver();
+        if (driver == null) {
+            throw new IllegalArgumentException("Driver must be set");
         }
 
-        @AfterClass
-            public void tearDown () {
-                DriverManager.closeDriver(); // Используем DriverManager для закрытия драйвера
-            }
-        }
+        // Инициализация WebDriverWait с общим временем ожидания
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Установите нужное время ожидания
+
+        webElementActions = new WebElementActions();
+        helper = new ImportHelpers(driver);
+        demoqaPages = new demoqa.module_import.DemoqaPages();
+        selectMenuPage = new SelectMenuPage();
+    }
+
+    // Метод для ожидания элемента
+    public void waitForElement(By by) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        DriverManager.closeDriver(); // Используем DriverManager для закрытия драйвера
+    }
+}
