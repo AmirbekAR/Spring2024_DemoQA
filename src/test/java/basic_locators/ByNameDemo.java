fd4e76c0c9068b24bf6bb947654f1c5ac389f39b
Amirbek <arynbaevamirbisnes@gmail.com>
2024-10-31 16:09:41 +0600

    some changes for test

--- src/test/java/basic_locators/ByNameDemo.java
+++ src/test/java/basic_locators/ByNameDemo.java
@@ -17,25 +17,6 @@ public class ByNameDemo extends BaseTest {
     public void demo1() throws InterruptedException {
 
        helper.browserManager.openURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-
-//        WebElement userNameInput = driver.findElement(By.name("username"));
-//        Thread.sleep(4000);
-//        userNameInput.sendKeys("Admin");
-//        WebElement passwordInput = driver.findElement(By.name("password"));
-//        Thread.sleep(4000);
-//        passwordInput.sendKeys("admin123");
-//        Thread.sleep(4000);
-//        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
-//        loginButton.click();
-//        Thread.sleep(4000);
-//        String expectedTitle = "Dashboard";
-//        WebElement title = driver.findElement(By.xpath("//h6"));
-//        Thread.sleep(4000);
-//        String actualTitle = title.getText();
-//        System.out.println(actualTitle);
-//        Assert.assertEquals(actualTitle, expectedTitle);
-//        driver.close();
-
         demoqaPages.byNameDemoPage.userName.sendKeys("Admin");
         demoqaPages.byNameDemoPage.password.sendKeys("admin123" + Keys.ENTER);
         Thread.sleep(4000);
